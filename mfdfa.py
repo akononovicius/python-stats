8cb89b54cd50cd30d49049ac0f5e0738c8d41485
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2018-10-04 09:53:03 +0300

    MFDFA estimation: change naming of functions

--- mfdfa.py
+++ mfdfa.py
@@ -9,7 +9,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def fluctuations(profile,q,scale):
+def __fluctuations(profile,q,scale):
     # segment profile into equal chunks
     segments=int(len(profile) // scale)
     stridedProfile=np.lib.stride_tricks.as_strided(profile,shape=(segments,scale))
@@ -22,19 +22,19 @@ def fluctuations(profile,q,scale):
         fqs[i]=np.mean((segVals-fitVals)**2)
     return fqs
 
-def dfa(profile,q,scaleSample,showFqs=False):
+def MakeDfa(profile,q,scaleSample,showFqs=False):
     # sample fluctuations in given points
     fqs=np.zeros(len(scaleSample))
     for i, s in enumerate(scaleSample):
         if(q!=0):
-            fqs[i]=np.mean(fluctuations(profile,q,s)**(q/2))**(1/q)
+            fqs[i]=np.mean(__fluctuations(profile,q,s)**(q/2))**(1/q)
         else:
-            fqs[i]=np.exp(0.5*np.mean(np.log(fluctuations(profile,q,s))))
+            fqs[i]=np.exp(0.5*np.mean(np.log(__fluctuations(profile,q,s))))
     if(showFqs):
         plt.plot(scaleSample,fqs,label="q="+str(q))
     return fqs
 
-def mfdfa(series,qSample,scaleSample,showFqs=False):
+def MakeMfDfa(series,qSample,scaleSample,showFqs=False):
     # obtain profile
     profile=np.cumsum(series-np.mean(series))
     logScaleSample=np.log10(scaleSample)
@@ -45,16 +45,16 @@ def mfdfa(series,qSample,scaleSample,showFqs=False):
         plt.xlabel('s')
         plt.ylabel('Fq(s)')
     for i, q in enumerate(qSample):
-        logFqs=np.log10(dfa(profile,q,scaleSample,showFqs=showFqs))
+        logFqs=np.log10(MakeDfa(profile,q,scaleSample,showFqs=showFqs))
         hq[i]=np.polyfit(logScaleSample,logFqs,1)[0]
     if(showFqs):
         plt.show()
     return hq
 
-def segMfdfa(series,qSample,scaleSample,segmentSize=None):
+def MakeSegMfDfa(series,qSample,scaleSample,segmentSize=None):
     if(segmentSize is None):
-        return mfdfa(series,qSample,scaleSample)
+        return MakeMfDfa(series,qSample,scaleSample)
     hqs=()
     for i in np.arange(0,len(series)-segmentSize+1,segmentSize):
-        hqs=hqs+(mfdfa(series[i:i+segmentSize],qSample,scaleSample),)
+        hqs=hqs+(MakeMfDfa(series[i:i+segmentSize],qSample,scaleSample),)
     return np.mean(np.vstack(hqs),axis=0)
