f45a573a30f6e1bcbf320af32c65f5ab9ab4c3ca
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2024-07-04 10:24:54 +0300

    beta_fit: add function to naively fit Beta(-binomial) distribution

---
+++ beta_fit.py
@@ -0,0 +1,39 @@
+from typing import Optional, Tuple
+
+import numpy as np
+
+
+def naive_beta_binomial_fit(
+    data: list, n: Optional[int] = None
+) -> Tuple[float, float, float]:
+    """Estimate Beta-binomial distribution parameters from the mean and variance of the data.
+
+    Input:
+        data:
+            List (array) containing data values. Logic dictates that it should
+            contain positive integer values, as otherwise fitting Beta-binomial
+            distribution makes no sense.
+        n: (default: None)
+            N parameter of the Beta-binomial distribution can be fixed by
+            passing this optional value. If `None` is passed (which is the
+            default), then the maximum of the `data` will be used as the
+            estimate for N parameter.
+
+    Output:
+        alpha, beta and N parameter estimates.
+    """
+    if n is None:
+        n = np.max(data)
+
+    mean = np.mean(data)
+    variance = np.var(data, ddof=1)
+
+    # these formulas were obtained by inverting the expressions for mean and
+    # variance of the Beta-binomial distribution (e.g., see
+    # https://en.wikipedia.org/wiki/Beta-binomial_distribution)
+    alpha_par = ((mean**2) * n - mean**3 - mean * variance) / (
+        mean**2 - mean * n + n * variance
+    )
+    beta_par = alpha_par * (n / mean - 1)
+
+    return alpha_par, beta_par, n
