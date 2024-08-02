515247e148b7c45fe9791eaa470a638e08a22e01
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2024-08-02 14:54:31 +0300

    CDF: replace np.histogram with right_histogram

--- cdf.py
+++ cdf.py
@@ -2,9 +2,11 @@ from typing import Optional
 
 import numpy as np
 
+from .histogram import right_histogram
+
 
 def make_cdf(
-    data: list,
+    data: np.ndarray,
     start: Optional[float] = None,
     stop: Optional[float] = None,
     out_points: int = 100,
@@ -14,8 +16,9 @@ def make_cdf(
         start = np.min(data)
     if stop is None:
         stop = np.max(data)
-    bins = np.linspace(start, stop, num=out_points)
-    histogram, _ = np.histogram(data, bins=bins, density=False)
-    cdf = np.vstack([bins, np.append([0], np.cumsum(histogram))]).T
+    histogram, edges = right_histogram(
+        data, start=start, stop=stop, out_points=out_points, density=False
+    )
+    cdf = np.vstack([edges, np.cumsum(histogram)]).T
     cdf[:, 1] /= np.sum(histogram)
     return cdf
