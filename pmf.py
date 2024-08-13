922fcd41079190076821194677572be3783fbe3d
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2024-08-13 11:13:58 +0300

    pmf: simplify mask logic

--- pmf.py
+++ pmf.py
@@ -12,15 +12,11 @@ def make_pmf(
     """Extract empirical probability mass function from data."""
     _data = np.array(data)
 
-    mask_flag = False
     if start is None:
         start = np.min(data)
-        mask_flag = True
     if stop is None:
         stop = np.max(data)
-        mask_flag = True
-    if mask_flag:
-        _data = _data[(start <= _data) & (_data <= stop)]
+    _data = _data[(start <= _data) & (_data <= stop)]
 
     counter_dict = Counter(_data)
     pmf = np.array(
