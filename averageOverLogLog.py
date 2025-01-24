da06230974eccaf8a4efcb51cc1d85c188657710
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2025-01-24 12:14:16 +0200

    averageOverLogLog: refactor

--- averageOverLogLog.py
+++ averageOverLogLog.py
@@ -1,39 +1,37 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from typing import Tuple
 
-import math
 import numpy as np
 
-#averaging of log-log stats
-def AverageOverLogLog(arrs,outPoints=100):
-    def _GetBinWidth(arr,i):
-        before=0
-        if(i>0):
-            before=arr[i-1,0]
-        nex=arr[i,0]
-        if(i<len(arr)-1):
-            nex=arr[i+1,0]
-        return (nex-before)*0.5
-    def _ResampleArea(arr,bins):
-        rez=[]    
-        i=0
-        for b in bins:
-            integral=0
-            tWidth=0
-            while(i<len(arr) and arr[i,0]<b):
-                width=_GetBinWidth(arr,i)
-                tWidth=tWidth+width
-                integral=integral+arr[i,1]*width
-                i=i+1
-            if(tWidth>0):
-                rez=rez+[integral/tWidth,]
-            else:
-                rez=rez+[0,]
-        return np.array(rez)
-    minX=np.min([np.min(arrs[i][:,0]) for i in range(len(arrs))])
-    maxX=np.max([np.max(arrs[i][:,0]) for i in range(len(arrs))])
-    steps=np.logspace(math.log10(minX),math.log10(maxX),outPoints)
-    rez=np.mean(np.array([_ResampleArea(arr,steps) for arr in arrs]),axis=0)
-    steps=steps*math.sqrt(steps[0]/steps[1])
-    rez=np.vstack((steps,rez)).T
-    return rez[rez[:,1]>0]
+
+def average_over_loglog(arrs: Tuple[np.ndarray], out_points: int = 100) -> np.ndarray:
+    """Average arrays with log-log data."""
+
+    def _get_bin_width(data: np.ndarray, idx: int) -> float:
+        # Bin width is estimated to be half of the
+        # interval between surrounding points.
+        before = data[idx - 1, 0] if (idx > 0) else 0.0
+        next = data[idx + 1, 0] if (idx < len(data) - 1) else data[idx, 0]
+        return (next - before) / 2.0
+
+    def _resample_area(data: np.ndarray, poles: np.ndarray) -> np.ndarray:
+        areas = np.zeros(len(poles))
+        data_idx = 0
+        for idx, pole in enumerate(poles):
+            integral = 0.0
+            total_width = 0.0
+            while data_idx < len(data) and data[data_idx, 0] < pole:
+                width = _get_bin_width(data, data_idx)
+                total_width = total_width + width
+                integral = integral + data[data_idx, 1] * width
+                data_idx = data_idx + 1
+            if total_width > 0:
+                areas[idx] = integral / total_width
+        return areas
+
+    min_value = np.min([np.min(arr[:, 0]) for arr in arrs])
+    max_value = np.max([np.max(arr[:, 0]) for arr in arrs])
+    poles = np.logspace(np.log10(min_value), np.log10(max_value), out_points)
+    resampled = np.mean(np.array([_resample_area(arr, poles) for arr in arrs]), axis=0)
+    poles = poles * np.sqrt(poles[0] / poles[1])
+    resampled = np.vstack((poles, resampled)).T
+    return resampled[resampled[:, 1] > 0]
