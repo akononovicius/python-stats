4d7724bfbd1ff032b44c1b881ee0d1821643b238
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2025-01-24 18:01:55 +0200

    rename variables to clarify average_over_loglog

--- average_over_loglog.py
+++ average_over_loglog.py
@@ -13,10 +13,10 @@ def average_over_loglog(arrs: Tuple[np.ndarray], out_points: int = 100) -> np.nd
         next = data[idx + 1, 0] if (idx < len(data) - 1) else data[idx, 0]
         return (next - before) / 2.0
 
-    def _resample_area(data: np.ndarray, poles: np.ndarray) -> np.ndarray:
-        areas = np.zeros(len(poles))
+    def _resample_area(data: np.ndarray, x_poles: np.ndarray) -> np.ndarray:
+        areas = np.zeros(len(x_poles))
         data_idx = 0
-        for idx, pole in enumerate(poles):
+        for idx, pole in enumerate(x_poles):
             integral = 0.0
             total_width = 0.0
             while data_idx < len(data) and data[data_idx, 0] < pole:
@@ -28,10 +28,12 @@ def average_over_loglog(arrs: Tuple[np.ndarray], out_points: int = 100) -> np.nd
                 areas[idx] = integral / total_width
         return areas
 
-    min_value = np.min([np.min(arr[:, 0]) for arr in arrs])
-    max_value = np.max([np.max(arr[:, 0]) for arr in arrs])
-    poles = np.logspace(np.log10(min_value), np.log10(max_value), out_points)
-    resampled = np.mean(np.array([_resample_area(arr, poles) for arr in arrs]), axis=0)
-    poles = poles * np.sqrt(poles[0] / poles[1])
-    resampled = np.vstack((poles, resampled)).T
+    min_x = np.min([np.min(arr[:, 0]) for arr in arrs])
+    max_x = np.max([np.max(arr[:, 0]) for arr in arrs])
+    x_poles = np.logspace(np.log10(min_x), np.log10(max_x), out_points)
+    resampled = np.mean(
+        np.array([_resample_area(arr, x_poles) for arr in arrs]), axis=0
+    )
+    x_poles = x_poles * np.sqrt(x_poles[0] / x_poles[1])
+    resampled = np.vstack((x_poles, resampled)).T
     return resampled[resampled[:, 1] > 0]
