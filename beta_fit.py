7474e72613a55d50c5154543c2f01a0c2e29aa7c
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2025-01-27 14:04:16 +0200

    beta_fit: add ordinary naive fitting function

--- beta_fit.py
+++ beta_fit.py
@@ -37,3 +37,27 @@ def naive_beta_binomial_fit(
     beta_par = alpha_par * (n / mean - 1)
 
     return alpha_par, beta_par, n
+
+
+def naive_beta_fit(data: list) -> Tuple[float, float]:
+    """Estimate Beta distribution parameters from the mean and variance of the data.
+
+    Input:
+        data:
+            List (array) containing data values. Logic dictates that it should
+            contain values in (0, 1) range, as otherwise fitting Beta
+            distribution might result in nonsensical fits.
+
+    Output:
+        alpha, beta parameter estimates.
+    """
+    mean = np.mean(data)
+    variance = np.var(data, ddof=1)
+
+    # these formulas were obtained by inverting the expressions for mean and
+    # variance of the Beta-binomial distribution (e.g., see
+    # https://en.wikipedia.org/wiki/Beta_distribution)
+    alpha_par = ((mean**2) - mean**3 - mean * variance) / variance
+    beta_par = alpha_par * (1 / mean - 1)
+
+    return alpha_par, beta_par
